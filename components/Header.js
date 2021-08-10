22871207adc12c4ee3840b45f7d26e00cf5b6f69
IMPrimph <vishnugajulapalli123@gmail.com>
2021-08-10 16:12:34 +0530

    add date range picker and state for start and end dates

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import {
     SearchIcon,
@@ -7,8 +7,32 @@ import {
     UserCircleIcon,
     UsersIcon
 } from '@heroicons/react/solid'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range'
+
 
 function Header() {
+    const [searchInput, setSearchInput] = useState("")
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+    const [noOfGuests, setNoOfGuests] = useState(1)
+
+    const selectionRange = {
+        startDate,
+        endDate,
+        key: 'selection'
+    }
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
+    }
+
+    const resetInput = () => {
+        setSearchInput("")
+    }
+
     return (
         <header className='sticky top-0 z-50 
         grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
@@ -31,6 +55,8 @@ function Header() {
                     bg-transparent outline-none text-sm 
                     text-gray-600 placeholder-gray-400'
                     type="text"
+                    value={searchInput}
+                    onChange={e => setSearchInput(e.target.value)}
                     placeholder='Start your search'
                 />
                 <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white 
@@ -52,6 +78,45 @@ function Header() {
                     <UserCircleIcon className='h-6' />
                 </div>
             </div>
+
+            {searchInput && (
+                <div className='flex flex-col 
+                col-span-3 mx-auto mt-2'>
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        minDate={new Date()}
+                        rangeColors={['#fd5b61']}
+                        onChange={handleSelect}
+                    />
+                    <div className='flex items-center border-b mb-4'>
+                        <h2 className="text-2xl flex-grow font-semibold">
+                            Number of Guests
+                        </h2>
+                        <UsersIcon
+                            className='h-5'
+                        />
+                        <input
+                            type="number"
+                            min={1}
+                            className="w-12 pl-2 outline-none text-red-400"
+                            value={noOfGuests}
+                            onChange={e => setNoOfGuests(e.target.value)}
+                        />
+                    </div>
+                    <div className='flex'>
+                        <button
+                            className='flex-grow text-gray-500'
+                            onClick={resetInput}
+                        >
+                            Cancel
+                        </button>
+                        <button className='flex-grow text-red-400'>
+                            Search
+                        </button>
+                    </div>
+                </div>
+            )}
+
         </header>
     )
 }
---
+++ pages/search.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function Search() {
+    return (
+        <div>
+            <h1>I am the Search page</h1>
+        </div>
+    )
+}
+
+export default Search
